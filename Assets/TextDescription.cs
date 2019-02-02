c9357628d7e8305d8ffe8a88db8580fd9ccc53ff
Kas <ksg304@nyu.edu>
2019-02-01 23:44:43 -0500

    all the amplify stuff and my text doc descriptin

---
+++ Assets/TextDescription.cs
@@ -0,0 +1,8 @@
+﻿//HOW DOES IT WORK?
+//    Just make a tree and a shroom with your cool asss shader on them. 
+//    Tree needs a collider
+//    Mushroom needs some other stuff, just duplicate one of the prefabs to see. 
+//    When you place them into ForestGen, they should be in the same spots on their respective lists
+//    I.e. if your GreenGoo shader tree is number 6 in the tree prefabs list, GreenGoo shader shroom should be number 6 in shroom prefabs list
+//    That's it!
+//    hit play, WASD, click to pick up Shroom, left click to eat, right click to drop.
